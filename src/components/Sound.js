fed60d973ca85c37c37e8d3ff0addd4d61d2c3e4
Shy L <63942346+ShyLee873@users.noreply.github.com>
2026-03-17 17:45:26 -0400

    fixed sound issues

---
+++ src/components/Sound.js
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+
+export default function useSound() {
+  const sounds = useRef({
+    incorrect: new Audio('/sounds/incorrect.wav'),
+    correct: new Audio('/sounds/correct.wav'),
+    countdown: new Audio('/sounds/countdown-click.wav'),
+    fail: new Audio('/sounds/fail.mp3'),
+    win: new Audio('/sounds/victory.wav'),
+  });
+
+  const stopAudio = (soundName) => {
+    const audio = sounds.current[soundName];
+    if (audio) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  };
+
+  const stopAllSounds = () => {
+    Object.values(sounds.current).forEach((audio) => {
+      audio.pause();
+      audio.currentTime();
+    });
+  };
+
+  const playSound = (soundName) => {
+    stopAudio(soundName);
+    sounds.current[soundName]?.play().catch((error) => {
+      console.error(`Could not play ${soundName}: `, error);
+    });
+  };
+
+  return { playSound, stopAudio, stopAllSounds };
+}
